c4699236286b3161b575ee7b44a119932bc3eff8
Bahman Pournazari <bahmanpn@gmail.com>
2023-04-06 20:52:19 +0430

    3-1 added room template and set view and url

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home-page'),
+    path('chat/<str:room_name>/', room, name='room-page'),
 ]
--- chat_app/views.py
+++ chat_app/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "chat_app/index.html")
+
+
+# room_name is getting from index page with jquery codes that set roomName and when we search roomname it help us
+# to transfer our search to room view and view get our search
+def room(request, room_name):
+    context = {
+        "room_name": room_name
+    }
+    return render(request, "chat_app/room.html", context)
