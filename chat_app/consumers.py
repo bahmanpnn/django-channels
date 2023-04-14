ec42524b06e3196b199660e86f467b80c1741cc5
Bahman Pournazari <bahmanpn@gmail.com>
2023-04-14 13:59:56 +0430

    8-completed fetch message method and new_message method and added new template

--- chat_app/consumers.py
+++ chat_app/consumers.py
@@ -1,27 +1,44 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer, WebsocketConsumer
 from asgiref.sync import async_to_sync
+from django.contrib.auth import get_user_model
 from rest_framework.renderers import JSONRenderer
 
 from .serializers import MessagesSerializer
 from .models import Message
 
+user = get_user_model()
+
 
 class ChatConsumer(WebsocketConsumer):
     def new_message(self, data):
-        pass
+        message = data['message']
+        author = data['username']
+        user_model = user.objects.filter(username=author).first()
+        new_message_model = Message.objects.create(author=user_model, content=message)
+        result = eval(self.message_serializer(new_message_model))
+        # result = self.message_serializer(new_message_model)
+        # result = eval(result)['content']
+        self.send_to_chat_message(result)
 
-    def fetch_messages(self,data):
+    def fetch_messages(self, data):
         query_set = Message.last_message(self)
         json_message = self.message_serializer(query_set)
         content = {
-            'message': eval(json_message)  # eval() help us to django does not check data is bytes_data or no
+            'message': eval(json_message),  # eval() help us to django does not check data is bytes_data or no
+            'command': 'fetch_message'
         }
         # now we must send content dic to chat_message method
         self.chat_message(content)
 
     def message_serializer(self, queryset):
-        serialized = MessagesSerializer(queryset, many=True)
+        # queryset.__class__.__name_ when is from fetch method is Queryset other time(new_message method) is
+        # new message model type from new_message method and lamda will be false!
+        # print((lambda queryset: True if (queryset.__class__.__name__ == 'QuerySet') else False)(queryset))
+
+        serialized = MessagesSerializer(queryset, many=(
+            lambda queryset: True if (queryset.__class__.__name__ == 'QuerySet') else False)(queryset)
+                                        )
         content = JSONRenderer().render(serialized.data)
         return content
 
@@ -49,29 +66,36 @@ class ChatConsumer(WebsocketConsumer):
 
     def receive(self, text_data=None, bytes_data=None):
         text_data_dict = json.loads(text_data)
-        message = text_data_dict.get('message', None)
-
+        # message = text_data_dict.get('message', None)
+        # username = text_data_dict.get('username', None)
         # this part of code is using for choose fetch_message method or new_message method
         command = text_data_dict['command']
-        self.commands['command'](self, message)
+        self.commands['command'](self, text_data_dict)
 
-        def send_to_chat_message(self, message):
-            async_to_sync(self.channel_layer.group_send)(
-                self.room_group_name,
-                {
-                    'type': 'chat_message',
-                    'message': message
-                }
-            )
+    def send_to_chat_message(self, message):
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                # 'message': message,
+                'content':message['content'],
+                'command': 'new_message',
+                '__str__':message['__str__'],
+            }
+        )
 
     def chat_message(self, event):
-        message = event['message']
+        self.send(text_data=json.dumps(event))
+        # message = event['message']
         # self.send() resend data to websocket to echo the message and show in textarea and room page
 
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        # self.send(text_data=json.dumps({
+        #     'message': message
+        # }))
 
+# _____________________________________________________________________________________________________
+
+#
 # class ChatConsumer(AsyncWebsocketConsumer):
 #     async def connect(self):
 #         self.room_name = self.scope['url_route']['kwargs']['room_name']
--- chat_app/views.py
+++ chat_app/views.py
@@ -1,4 +1,7 @@
+import json
+
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
 
 
 # Create your views here.
@@ -9,7 +12,9 @@ def index(request):
 # room_name is getting from index page with jquery codes that set roomName and when we search roomname it help us
 # to transfer our search to room view and view get our search
 def room(request, room_name):
+    username = request.user.username
     context = {
-        "room_name": room_name
+        "room_name": room_name,
+        "username": mark_safe(json.dumps(username))
     }
-    return render(request, "chat_app/room.html", context)
+    return render(request, "chat_app/oldroom.html", context)
